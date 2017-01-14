39c2757e3407538eca40ed735a4fd4694e3f127f
imcgonigle <ian.is.mcgonigle@gmail.com>
2017-01-14 16:08:55 -0700

    add RoundSettingsActivity with layout

---
+++ app/src/main/java/com/rockyourglock/android/shootingtimer/RoundSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.rockyourglock.android.shootingtimer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_settings);
+    }
+
+}
--- app/src/main/java/com/rockyourglock/android/shootingtimer/WarningActivity.java
+++ app/src/main/java/com/rockyourglock/android/shootingtimer/WarningActivity.java
@@ -3,6 +3,7 @@ package com.rockyourglock.android.shootingtimer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WarningActivity extends AppCompatActivity {
 
@@ -11,7 +12,8 @@ public class WarningActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warning);
     }
-    public void agreeToWarning(){
-
+    public void agreeToWarning(View view){
+        Intent intent = new Intent(this, RoundSettingsActivity.class);
+        startActivity(intent);
     }
 }
