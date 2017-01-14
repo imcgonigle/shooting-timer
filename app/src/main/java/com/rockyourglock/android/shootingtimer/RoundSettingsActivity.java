54f3f1fb505030f95f5839b236d0d34423dcd102
imcgonigle <ian.is.mcgonigle@gmail.com>
2017-01-14 16:19:10 -0700

    add view to toRound method parameters

--- app/src/main/java/com/rockyourglock/android/shootingtimer/RoundSettingsActivity.java
+++ app/src/main/java/com/rockyourglock/android/shootingtimer/RoundSettingsActivity.java
@@ -3,6 +3,7 @@ package com.rockyourglock.android.shootingtimer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RoundSettingsActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class RoundSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_round_settings);
     }
 
-    public void toRound() {
+    public void toRound(View view) {
         Intent intent = new Intent(this, RoundActivity.class);
         startActivity(intent);
     }
