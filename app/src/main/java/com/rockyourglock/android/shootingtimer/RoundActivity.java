d11603ab84e180f8c0a769b8f4925b43707a6be6
imcgonigle <ian.is.mcgonigle@gmail.com>
2017-01-14 16:24:37 -0700

    add backToSettings method to go back to settings

--- app/src/main/java/com/rockyourglock/android/shootingtimer/RoundActivity.java
+++ app/src/main/java/com/rockyourglock/android/shootingtimer/RoundActivity.java
@@ -1,7 +1,9 @@
 package com.rockyourglock.android.shootingtimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RoundActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RoundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round);
+    }
+
+    public void backToSettings(View view){
+        Intent intent = new Intent(this, RoundSettingsActivity.class);
+        startActivity(intent);
     }
 }
