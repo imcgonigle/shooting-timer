d33ff9f305a3bbaee252a0e7f4527b56e6882242
imcgonigle <ian.is.mcgonigle@gmail.com>
2017-01-14 23:51:11 -0700

    finish most of RoundActivity

--- app/src/main/java/com/rockyourglock/android/shootingtimer/RoundActivity.java
+++ app/src/main/java/com/rockyourglock/android/shootingtimer/RoundActivity.java
@@ -5,32 +5,55 @@ import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class RoundActivity extends AppCompatActivity {
+
+    String[] sounds = {"Red", "Orange", "Yellow", "Blue", "Brown",
+                        "Grey", "Green", "Black", "Purple", "Triangle",
+                        "Square", "Star", "Arrow", "Diamond", "Oval",
+                        "Circle", "Rectangle", "Heart"};
 
     Button startButton;
     Button pauseButton;
     Button resumeButton;
 
     TextView timerValue;
+    TextView targetValue;
 
     CountDownTimer countDownTimer;
 
-    final int timeInSeconds = 13;
+    int timeInSeconds;
     int millisecondsLeft;
+    int startInSeconds;
+    int timeBetweenItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round);
 
+        Intent intent = getIntent();
+        timeInSeconds  = Integer.parseInt(intent
+                .getStringExtra(RoundSettingsActivity.EXTRA_DURATION_VALUE));
+        startInSeconds = Integer.parseInt(intent
+                .getStringExtra(RoundSettingsActivity.EXTRA_STARTIN_VALUE));
+        timeBetweenItems = (int) (Double.parseDouble(intent
+                        .getStringExtra(RoundSettingsActivity.EXTRA_TIME_BETWEEN_VALUE)) * 1000);
+
         startButton = (Button) findViewById(R.id.btn_start_round);
         pauseButton = (Button) findViewById(R.id.btn_stop);
         resumeButton = (Button) findViewById(R.id.btn_resume);
         timerValue = (TextView) findViewById(R.id.tv_timer);
+        targetValue = (TextView) findViewById(R.id.tv_target);
+
+        timerValue.setVisibility(View.VISIBLE);
+        timerValue.setText(Integer.toString(timeBetweenItems));
     }
 
     public void backToSettings(View view){
@@ -39,13 +62,30 @@ public class RoundActivity extends AppCompatActivity {
     }
 
     public void startRound(View view) {
-        int timeLength = timeInSeconds * 1000;
+        int startLength = startInSeconds * 1000;
+
+        CountDownTimer startTimer;
 
         startButton.setVisibility(View.INVISIBLE);
         pauseButton.setVisibility(View.VISIBLE);
         timerValue.setVisibility(View.VISIBLE);
-        setUpTimer(timeLength);
-        countDownTimer.start();
+        timerValue.setText(Integer.toString(startInSeconds + 1));
+        startTimer = new CountDownTimer(startLength, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int secondsUntilStart = (int) millisUntilFinished / 1000;
+                timerValue.setText(Integer.toString(secondsUntilStart + 1));
+            }
+
+            @Override
+            public void onFinish() {
+                int timeLength = timeInSeconds * 1000;
+                targetValue.setVisibility(View.VISIBLE);
+                setUpTimer(timeLength);
+                countDownTimer.start();
+            }
+        };
+        startTimer.start();
     }
 
     public void pauseRound(View view) {
@@ -66,6 +106,11 @@ public class RoundActivity extends AppCompatActivity {
             @Override
             public void onTick(long millisUntilFinished) {
                 millisecondsLeft = (int) (millisUntilFinished);
+                if(millisecondsLeft % timeBetweenItems <= 10){
+                    int rnd = new Random().nextInt(sounds.length);
+                    String targetItem = sounds[rnd];
+                    targetValue.setText(targetItem);
+                }
                 int seconds = millisecondsLeft / 1000;
                 int minutes = seconds / 60;
                 seconds = seconds % 60;
@@ -80,7 +125,9 @@ public class RoundActivity extends AppCompatActivity {
             public void onFinish() {
                 pauseButton.setVisibility(View.INVISIBLE);
                 resumeButton.setVisibility(View.INVISIBLE);
+                targetValue.setVisibility(View.INVISIBLE);
                 timerValue.setText("Finished");
+                targetValue.setText("Ready?");
                 startButton.setText("New Round");
                 startButton.setVisibility(View.VISIBLE);
             }
--- app/src/main/java/com/rockyourglock/android/shootingtimer/RoundSettingsActivity.java
+++ app/src/main/java/com/rockyourglock/android/shootingtimer/RoundSettingsActivity.java
@@ -4,17 +4,126 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RoundSettingsActivity extends AppCompatActivity {
+    public final static String EXTRA_DURATION_VALUE = "com.rockyourglock.android.shootingtimer.DURATION";
+    public final static String EXTRA_STARTIN_VALUE = "com.rockyourglock.android.shootingtimer.STARTIN";
+    public final static String EXTRA_TIME_BETWEEN_VALUE = "com.rockyourglock.android.shootingtimer.TIME_BETWEEN";
+
+    private SeekBar roundDuratoin;
+    private TextView durationValueTextView;
+
+    private SeekBar startIn;
+    private TextView startInTextView;
+
+    private SeekBar timeBetweenItems;
+    private TextView timeBetweenItemsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_settings);
+        initializeVariables();
+        int seconds = roundDuratoin.getProgress() * 15 + 15;
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        // Initialize the durationValue with '15 sec'.
+        durationValueTextView.setText("min " + String.format("%02d", minutes)
+                + " sec " + String.format("%02d", seconds));
+        startInTextView.setText("1 sec");
+        timeBetweenItemsTextView.setText(".4 sec");
+
+        roundDuratoin.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbars progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int seconds = progress * 15 + 15;
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+                durationValueTextView.setText("min " + String.format("%02d", minutes)
+                        + " sec " + Integer.toString(seconds) );
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        startIn.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbars progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int seconds = progress + 1;
+                startInTextView.setText("" + String.format("%02d", seconds) + " sec");
+            }
+        });
+
+        timeBetweenItems.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                double time  = progress * .1 + .4;
+                timeBetweenItemsTextView.setText("" + String.format("%.1f", time) + " sec");
+            }
+        });
     }
 
     public void toRound(View view) {
+        String secondsString = String.valueOf(roundDuratoin.getProgress() * 15 + 15);
+        String startInExtra = String.valueOf(startIn.getProgress() + 1);
+        String timeBetweenExtra = String.valueOf(timeBetweenItems.getProgress() * .1 + .4);
+
         Intent intent = new Intent(this, RoundActivity.class);
+        intent.putExtra(EXTRA_DURATION_VALUE, secondsString);
+        intent.putExtra(EXTRA_STARTIN_VALUE, startInExtra);
+        intent.putExtra(EXTRA_TIME_BETWEEN_VALUE, timeBetweenExtra);
         startActivity(intent);
+    }
+
+    private void initializeVariables() {
+        roundDuratoin = (SeekBar) findViewById(R.id.sb_round_duration);
+        durationValueTextView = (TextView) findViewById(R.id.tv_duration_value);
+
+        startIn = (SeekBar) findViewById(R.id.sb_start_in);
+        startInTextView = (TextView) findViewById(R.id.tv_start_in_value);
+
+        timeBetweenItems = (SeekBar) findViewById(R.id.sb_time_to_value);
+        timeBetweenItemsTextView = (TextView) findViewById(R.id.tv_time_to_item_value);
     }
 }
